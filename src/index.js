6d90ad826693f3cab5e717aa67aa1a1fff87286c
Nick Blackwell <nickblackwell82@gmail.com>
2023-01-04 12:37:50 -0800

    implement basic graph

--- src/index.js
+++ src/index.js
@@ -1,13 +1,132 @@
-import _ from 'lodash';
 
- function component() {
-   const element = document.createElement('div');
+(function(){
 
-  // Lodash, currently included via a script, is required for this line to work
-  // Lodash, now imported by this script
-   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-   return element;
- }
+	class Element{
 
- document.body.appendChild(component());
+		constructor(type, options){
+
+			var el=document.createElement(type);
+			Object.keys(options||{}).forEach((key)=>{
+
+				var v=options[k];
+
+				if(key=='html'){
+					el.innerHTML=v;
+					return;
+				}
+
+				if(key=='events'){
+					Object.keys(v).forEach((event)=>{
+						el.addEventListener(event, v[event]);
+					});
+					return;
+				}
+
+				if(key=='class'){
+					
+					el.classList.add(v);
+					return;
+				}
+
+			})
+
+
+		}
+
+
+	}
+
+	class Graph{
+
+		constructor(id){
+
+			this._container=document.document.getElementById(id);
+			this._element=new Element('div', {"class":"graph-root"})
+			this._container.appendChild(this._element);
+
+		}
+
+		addNode(data){
+
+			if(!this._nodes){
+				this._nodes=[];
+			}
+
+			var node=new Node(this, data);
+			this._nodes.push(node);
+			return node;
+
+		}
+
+		getElement(){
+
+			return this._element;
+
+		}
+
+	}
+
+
+	class Node{
+
+
+		constructor(parent, data){
+
+			this._parent=parent;
+			this._element=new Element('div', 'graph-node');
+			this._parent.appendChild(this._element);
+		}
+
+		getParent(){
+
+			return this.parent||null;
+
+		}
+
+		getElement(){
+
+			return this._element;
+
+		}
+
+
+		addNode(data){
+
+			if(!this._nodes){
+				this._nodes=[];
+			}
+
+			var node=new Node(this, data);
+			this._nodes.push(node);
+			return node;
+
+		}
+
+
+	}
+
+
+
+	var graph=(new Graph('survey-builder'));
+
+	graph.addNode({
+
+		elements:[
+			new Element('button', {
+				html:"Add Question",
+				events:{
+					click:function(){
+
+					}
+				}
+			})
+		]
+
+
+	})
+
+})();
+
+
+
